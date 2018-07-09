3d256ebc1b9bff1dfdfecd05fa36982462d3c74e
pBouillon <pro.pierre.bouillon@protonmail.com>
2018-07-09 15:21:26 +0200

    Code cleanup
    Signed-off-by: pBouillon <pro.pierre.bouillon@protonmail.com>

--- src/CarreMagic.java
+++ src/CarreMagic.java
@@ -1,114 +1,113 @@
 package src;
 
-import src.Terminal;
+// prog qui vérifie un carré magique
 
-//prog qui v�rifie un carr� magique
 /* 
- * Le carr� commence � la ligne et colonne 0
+ * Le carré commence à la ligne et colonne 0
 */
 public class CarreMagic{
-  public static void main(String[] args){
-    boolean check=true;
+    public static void main(String[] args){
+        boolean check;
 
- //tableau - � composer par l'utilisateur 
+        //tableau - à composer par l'utilisateur
 
-    int cases, nbmax;
-    Terminal.ecrireString("Choisissez la grandeur de votre carr�. exemple : 3 pour un carr� de 3 x 3");
-    Terminal.sautDeLigne();
-    cases = Terminal.lireInt();
-    nbmax = cases * cases;
-    Terminal.sautDeLigne();
-    Terminal.ecrireString("Remplissez le tableau et v�rifier si votre carr� est magique. La premi�re colonne et premi�re ligne commence � 0. Vous ne pouvez pas utiliser un nombre qui a d�j� �t� entr�");
-    Terminal.sautDeLigne();
+        int cases, nbmax;
+        Terminal.ecrireString("Choisissez la grandeur de votre carré. exemple : 3 pour un carré de 3 x 3.");
+        Terminal.sautDeLigne();
+        cases = Terminal.lireInt();
+        nbmax = cases * cases;
+        Terminal.sautDeLigne();
+        Terminal.ecrireString(
+                "Remplissez le tableau et vérifier si votre carré est magique. " +
+                "La première colonne et première ligne commence à 0. " +
+                "Vous ne pouvez pas utiliser un nombre qui a déjà été entré.");
+        Terminal.sautDeLigne();
 
     int[][] carre = new int[cases][cases];
     int lig, col, nomb;
     boolean[] dejautilise = new boolean[10];
-    for (int num=0; num<nbmax; num=num+1){
-      do{
-        do{
-          Terminal.ecrireString("Pour quelle colonne ? ");
-          col = Terminal.lireInt();
-        }while(col<0 || col>2);
-        do{
-          Terminal.ecrireString("quelle ligne ? ");
-          lig = Terminal.lireInt();
-        }while(lig<0 || lig>2);
-      }while(carre[col][lig]!=0);
-      do{
-        Terminal.ecrireString("Entrez un nombre : ");
-        nomb = Terminal.lireInt();
-      }while(nomb<1 || nomb>nbmax || dejautilise[nomb]);
-      carre[col][lig]=nomb;
-      dejautilise[nomb]=true;
-      for (int l=0; l<cases; l++){
-        for (int c=0; c<cases; c++){
-          if (carre[c][l]!=0){
-            Terminal.ecrireString(" "+carre[c][l]+"|");
-          }else{
-            Terminal.ecrireString(" |");
-          }
+
+    for (int num = 0; num < nbmax; num++) {
+        do {
+            do {
+                Terminal.ecrireString("Pour quelle colonne ? ");
+                col = Terminal.lireInt();
+            } while(col < 0 || col > 2);
+            do {
+                Terminal.ecrireString("Quelle ligne ? ");
+                lig = Terminal.lireInt();
+            } while(lig < 0 || lig > 2);
+        } while(carre[col][lig] != 0);
+        do {
+            Terminal.ecrireString("Entrez un nombre : ");
+            nomb = Terminal.lireInt();
+        } while(nomb < 1 || nomb > nbmax || dejautilise[nomb]);
+        carre[col][lig] = nomb;
+        dejautilise[nomb] = true;
+
+        for (int l=0; l<cases; l++) {
+            for (int c = 0; c < cases; c++) {
+                if (carre[c][l] != 0) {
+                    Terminal.ecrireString(" " + carre[c][l] + "|");
+                } else {
+                    Terminal.ecrireString(" |");
+                }
+            }
+            Terminal.sautDeLigne();
         }
-        Terminal.sautDeLigne();
-      }
     }
     
-//check diagonales
-    int som1=0,  som2=0;
-    for(int i=0; i<cases;i++){
-      som1=som1+carre[i][i];
+    //check diagonales
+    int som1 = 0, som2 = 0;
+    for (int i = 0; i < cases; i++) {
+        som1 += carre[i][i];
     }
-    int mavar=2 ;
-    for(int i=0; i<cases && (mavar>=0);i++){
-      som2=som2+carre[i][mavar];
+
+    int mavar = 2 ;
+    for (int i=0; i < cases && mavar >= 0; i++) {
+      som2 += carre[i][mavar];
       mavar--;        
     }
-    if (som2!=som1){
-      check=false;
-    }else{
-      check=true;
-    }
+
+    check = som2 == som1;
     
     
-//check ligne par ligne et colonne par colonne
-    int nb=0;
-    for(int i=0;i<carre.length;i++){
-      int som3=0, som4=0;
-      for(int j=0;j<carre[carre.length-1].length;j++){
-        som3=som3+carre[i][j];//addition pour le total par ligne
-        som4=som4+carre[j][i];//addition pour le total par colonne
-      }
+    //check ligne par ligne et colonne par colonne
+    int nb = 0;
+    for (int i = 0; i < carre.length; i++) {
+        int som3 = 0, som4 = 0;
+        for (int j = 0; j < carre[carre.length-1].length; j++) {
+            som3 += carre[i][j];//addition pour le total par ligne
+            som4 += carre[j][i];//addition pour le total par colonne
+        }
       //src.Terminal.ecrireStringln("le total par ligne : "+som3);
       //src.Terminal.ecrireStringln("le total par colonne : "+som4);
       
-      if (som3!=som1 || som4!=som1 ){
-        nb++;
-      }else{
-        nb--;
-      }
+      if (som3 != som1 || som4 != som1) {
+            nb++;
+        } else {
+            nb--;
+        }
     }
-    if (nb<0){
-      check=true;
-    }else{
-      check=false;
-    }
+
+    check = nb < 0;
     
-//affichage du check : si c'est un carr� magique ou pas
+    //affichage du check : si c'est un carré magique ou pas
     if (check){
-      Terminal.ecrireString("Bravo ! C'est un carr� magique.");
+      Terminal.ecrireString("Bravo ! C'est un carré magique.");
     }else{
-      Terminal.ecrireString("Ce n'est pas un carr� magique. Essai encore une fois!");  
+      Terminal.ecrireString("Ce n'est pas un carré magique. Essai encore une fois!");
     }
   }
 }
 
 
 /*Jeu de test
- valeur � entrer champs par champs dans le prog : 
- 0,0,2,1,0,7,2,0,6,0,1,5,1,1,9,2,1,1,0,2,4,1,2,3,2,2,8 => FAUX ce n'est pas un carr� magique
- 0,0,2,1,0,7,2,0,6,0,1,9,1,1,5,2,1,1,0,2,4,1,2,3,2,2,8 => vrai c'est un carr� magique
+ valeur à entrer champs par champs dans le prog :
+ 0,0,2,1,0,7,2,0,6,0,1,5,1,1,9,2,1,1,0,2,4,1,2,3,2,2,8 => FAUX ce n'est pas un carré magique
+ 0,0,2,1,0,7,2,0,6,0,1,9,1,1,5,2,1,1,0,2,4,1,2,3,2,2,8 => vrai c'est un carré magique
  
- ou valeur � entrer dans votre code
- {2,7,6}, {5,9,1}, {4,3,8} => FAUX ce n'est pas un carr� magique
- {2,7,6}, {9,5,1}, {4,3,8} => vrai c'est un carr� magique
+ ou valeur à entrer dans votre code
+ {2,7,6}, {5,9,1}, {4,3,8} => FAUX ce n'est pas un carré magique
+ {2,7,6}, {9,5,1}, {4,3,8} => vrai c'est un carré magique
  */
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,131 +1,131 @@
 package src;
 
 import java.io.*;
+
+
 public class Terminal{    
-    static BufferedReader in =
-	new BufferedReader(new InputStreamReader(System.in));
-    public static String lireString() // Lire un String
-	{
-	    String tmp="";
-	    char C='\0';
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+    private static String lireString() { // Lire un String
+	    String tmp = "";
 	    try {
-		tmp = in.readLine();
+		    tmp = in.readLine();
 	    }
-	    catch (IOException e)
-	    {
-		exceptionHandler(e);
+	    catch (IOException e) {
+		    exceptionHandler(e);
 	    }
 	    return tmp;
 	} // fin de lireString()
-    public static int lireInt()  // Lire un entier
-	{
-	    int x=0;
+
+    static int lireInt() { // Lire un entier
+	    int x = 0;
 	    try {
-		x=Integer.parseInt(lireString());
+		    x = Integer.parseInt(lireString());
 	    }
 	    catch (NumberFormatException e) {
-		exceptionHandler(e);
+		    exceptionHandler(e);
 	    }	
 	    return x ;
 	}
-    public static boolean lireBoolean()  // Lire un entier
-	{
+
+    public static boolean lireBoolean() { // Lire un entier
 	    boolean b = true;
 	    try {
-		b = Boolean.valueOf(lireString()).booleanValue();
+		    b = Boolean.valueOf(lireString());
 	    }
 	    catch (NumberFormatException e) {
-		exceptionHandler(e);
+		    exceptionHandler(e);
 	    }	
 	    return b;
 	}
-    public  static double lireDouble()  // Lire un double
-	{
-	    double x=0.0;
+
+    public static double lireDouble() { // Lire un double
+	    double x = 0.0;
 	    try {
-		x=Double.valueOf(lireString()).doubleValue();
+		    x = Double.valueOf(lireString());
 	    }
 	    catch (NumberFormatException e) {
-		exceptionHandler(e);
+		    exceptionHandler(e);
 	    }	
 	    return x ;
 	}
-    public  static char lireChar()  // Lire un caractere
-	{
-	    String tmp=lireString();
+
+    public  static char lireChar() { // Lire un caractere
+	    String tmp = lireString();
 	    if (tmp.length()==0)
-		return '\n';
+		    return '\n';
 	    else 
-	    {
-		return tmp.charAt(0);
-	    }
+    		return tmp.charAt(0);
 	}
-    public static void ecrireString(String s){ // Afficher un String
-	try{
-	    System.out.print(s);
-	} catch (Exception ex){
-	    exceptionHandler(ex);
-	}
+
+	static void ecrireString(String s) { // Afficher un String
+        try {
+            System.out.print(s);
+        } catch (Exception ex){
+            exceptionHandler(ex);
+        }
     }
-    public static void ecrireStringln(String s) // Afficher un String
-	{
+
+    public static void ecrireStringln(String s) { // Afficher un String
 	    ecrireString(s);
 	    sautDeLigne();
 	} // fin de ecrireStringln()
-    public static void ecrireInt(int i)  // Afficher un entier
-	{
-	    ecrireString(""+i);
+
+    public static void ecrireInt(int i) { // Afficher un entier
+	    ecrireString(String.valueOf(i));
 	}
-    public static void ecrireIntln(int i)  // Afficher un entier
-	{
-	    ecrireString(""+i);
+    public static void ecrireIntln(int i) { // Afficher un entier
+	    ecrireString(String.valueOf(i));
 	    sautDeLigne();
 	}
-    public static void ecrireBoolean(boolean b){
-	ecrireString(""+b);
+    public static void ecrireBoolean(boolean b) {
+	    ecrireString(String.valueOf(b));
     }
-    public static void ecrireBooleanln(boolean b){
-	ecrireString(""+b);
-	sautDeLigne();
+
+    public static void ecrireBooleanln(boolean b) {
+        ecrireString(String.valueOf(b));
+        sautDeLigne();
     }
-    public  static void ecrireDouble(double d)  // Afficher un double
-	{
-	    ecrireString(""+d);
+
+    private static void ecrireDouble(double d) { // Afficher un double
+	    ecrireString(String.valueOf(d));
 	}
-    public  static void ecrireDoubleln(double d)  // Afficher un double
-	{
-	    ecrireDouble(d);
+    public  static void ecrireDoubleln(double d) { // Afficher un double
+        ecrireString(String.valueOf(d));
 	    sautDeLigne();
 	}
-    public  static void ecrireChar(char c)  // Afficher un caractere
-	{
-	    ecrireString(""+c);
-	}  
-    public  static void ecrireCharln(char c)  // Afficher un caractere
-	{
+
+    private static void ecrireChar(char c) { // Afficher un caractere
+	    ecrireString(String.valueOf(c));
+	}
+
+    public  static void ecrireCharln(char c) { // Afficher un caractere
 	    ecrireChar(c);
 	    sautDeLigne();
 	}
-    public static void sautDeLigne(){
-	try{
-	    System.out.println();
-	}catch(Exception ex){
-	    exceptionHandler(ex);
-	}
+
+    static void sautDeLigne(){
+	    try{
+	        System.out.println();
+	    }catch(Exception ex){
+	        exceptionHandler(ex);
+	    }
     }
-    protected static void exceptionHandler(Exception ex){
-	TerminalException err = new TerminalException(ex);
-	throw err;
+
+    private static void exceptionHandler(Exception ex) {
+        throw new TerminalException(ex);
     }
-    public static void ecrireException(Throwable ex){
-	ecrireString(ex.toString());
-	ex.printStackTrace(System.err);
+
+    public static void ecrireException(Throwable ex) {
+        ecrireString(ex.toString());
+        ex.printStackTrace(System.err);
     }
-}  
+}
+
 class TerminalException extends RuntimeException{
-    Exception ex;
+    private Exception ex;
     TerminalException(Exception e){
-	ex = e;
+	    ex = e;
     }
 }
 
